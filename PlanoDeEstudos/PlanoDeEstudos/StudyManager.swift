a75a23e651cb66929ca27afcdbe57802a2a3b57e
João Paulo Camargo Couto Queiroz <queirozjoao95@gmail.com>
2021-01-26 09:17:51 -0300

    Plano de estudos Finalizado

---
+++ PlanoDeEstudos/PlanoDeEstudos/StudyManager.swift
@@ -0,0 +1,45 @@
+//
+//  StudyManager.swift
+//  PlanoDeEstudos
+//
+//  Created by Joao Queiroz on 25/01/21.
+//  Copyright © 2021 Eric Brito. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class StudyManager{
+    static let shared = StudyManager()
+    let ud = UserDefaults.standard
+    var studyPlans: [StudyPlan] = []
+    
+    private init() {
+        if let data = ud.data(forKey: "studyPlans"), let plans = try? JSONDecoder().decode([StudyPlan].self, from: data) {
+            self.studyPlans = plans
+        }
+    }
+    
+    func savePlans(){
+        if let data = try? JSONEncoder().encode(studyPlans){
+            ud.set(data, forKey: "studyPlans")
+        }
+    }
+    
+    func addPlan(_ studyPlan: StudyPlan){
+        studyPlans.append(studyPlan)
+        savePlans()
+    }
+    
+    func removePlan(at index: Int){
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [studyPlans[index].id])
+        studyPlans.remove(at: index)
+        savePlans()
+    }
+    
+    func setPlanDone(id: String){
+        if let studyPlan = studyPlans.first(where: {$0.id == id}){
+            studyPlan.done = true
+        }
+    }
+}
