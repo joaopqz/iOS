0f0bea771003121150048d9c7c256149a01853ca
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-05-31 12:04:16 -0300

    ensure RemoteAddAccount calls HttpClient with correct URL

---
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,48 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by João Queiroz | Gerencianet on 31/05/21.
+//
+
+import XCTest
+
+class RemoteAddAccount{
+    
+    private let url: URL
+    private let httpClient: HttpClient
+    
+    init(url: URL, httpClient: HttpClient){
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    func add(){
+        httpClient.post(url: url)
+    }
+}
+
+protocol HttpClient {
+    
+    func post(url: URL)
+}
+
+class RemoteAddAccountTests: XCTestCase {
+
+    func test_() {
+        let url = URL(string: "http://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy) //system under test... classe a ser testada.
+        sut.add()
+        XCTAssertEqual(httpClientSpy.url, url)
+    }
+
+    class HttpClientSpy: HttpClient {
+        var url: URL?
+        
+        func post(url: URL){
+            self.url = url
+        }
+    }
+    
+}
