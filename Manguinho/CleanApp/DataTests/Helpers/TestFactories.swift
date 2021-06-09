5583210bd32c4d952300898a2166be6bb3637b61
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-09 11:39:38 -0300

    ensure AlamofireAdaoter calls Alamofire with correct data

--- Manguinho/CleanApp/DataTests/Helpers/TestFactories.swift
+++ Manguinho/CleanApp/DataTests/Helpers/TestFactories.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data{
     return Data("invalid_data".utf8)
 }
 
+func makeValidData() -> Data{
+    return Data("{\"name\":\"Joao\"}".utf8)
+}
+
 func makeURL() -> URL {
     return URL(string: "http://any-url.com")!
 }
