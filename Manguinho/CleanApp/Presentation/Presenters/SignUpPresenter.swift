409ff37ee7c86b481d892bcb25f6816691231ca4
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-11 10:30:41 -0300

    move presentation classes to their own files

---
+++ Manguinho/CleanApp/Presentation/Presenters/SignUpPresenter.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+public final class SignUpPresenter {
+    
+    private let alertView: AlertView
+    
+    public init (alertView: AlertView){
+        self.alertView = alertView
+    }
+    
+    public  func signUp(viewModel: SignUpViewModel){
+        if let message = validate(viewModel: viewModel) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
+        }
+    }
+    
+    private func validate(viewModel: SignUpViewModel) -> String? {
+        if viewModel.name == nil || viewModel.name!.isEmpty {
+            return "Campo NOME é obrigatório!!"
+        } else if viewModel.email == nil || viewModel.email!.isEmpty {
+            return "Campo EMAIL é obrigatório!!"
+        } else if viewModel.password == nil || viewModel.password!.isEmpty {
+             return  "Campo SENHA é obrigatório!!"
+        } else if viewModel.passwordConfimation == nil || viewModel.passwordConfimation!.isEmpty {
+            return "Campo CONFIRMAR SENHA é obrigatório!!"
+        }
+        return nil
+    }
+    
+}
+
+public struct SignUpViewModel{
+    public var name: String?
+    public var email: String?
+    public var password: String?
+    public var passwordConfimation: String?
+    
+    public init (name: String? = nil, email: String? = nil, password: String? = nil, passwordConfimation: String? = nil){
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfimation = passwordConfimation
+    }
+}
