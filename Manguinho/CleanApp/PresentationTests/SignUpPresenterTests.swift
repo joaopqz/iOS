d10a1655003d7c4a51476db863f0f1e0717cc8ab
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-11 09:10:34 -0300

    add presentation Layer

---
+++ Manguinho/CleanApp/PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by João Queiroz | Gerencianet on 11/06/21.
+//
+
+import XCTest
+@testable import Presentation
+
+class SignUpPresenterTests: XCTestCase {
+
+    func test_(){
+        
+    }
+
+}
