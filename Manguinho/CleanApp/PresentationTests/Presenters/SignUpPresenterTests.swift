ee5b23a31b685657ba5bbd5c886c74d3c47599cc
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-14 11:24:19 -0300

    ensure SignUpPresenter shows success message when SignUp succeeds

--- Manguinho/CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ Manguinho/CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -128,6 +128,20 @@ class SignUpPresenterTests: XCTestCase {
         wait(for: [exp2], timeout: 1)
     }
     
+    func test_signUp_should_show_success_message_if_addAccount_succeeds(){
+        let alertViewSpy = AlertViewSpy()
+        let addAccountSpy = AddAccountSpy()
+        let sut = makeSut(alertView: alertViewSpy, addAccount: addAccountSpy)
+        let exp = expectation(description: "waiting")
+        alertViewSpy.observe { [weak self] viewModel in
+            XCTAssertEqual(viewModel, self?.makeSuccessAlertViewModel(message: "Conta criada com sucesso!!"))
+            exp.fulfill()
+        }
+        sut.signUp(viewModel: makeSingUpViewModel())
+        addAccountSpy.completeWithAccount(makeAccountModel())
+        wait(for: [exp], timeout: 1)
+    }
+    
 }
 
 extension SignUpPresenterTests{
@@ -152,6 +166,10 @@ extension SignUpPresenterTests{
  
     func makeErrorAlertViewModel(message: String) -> AlertViewModel {
         return AlertViewModel(title: "Erro", message: message)
+    }
+    
+    func makeSuccessAlertViewModel(message: String) -> AlertViewModel {
+        return AlertViewModel(title: "Sucesso", message: message)
     }
     
     class AlertViewSpy: AlertView {
@@ -196,6 +214,11 @@ extension SignUpPresenterTests{
         func completeWithError(_ error: DomainError){
             completion?(.failure(error))
         }
+        
+        func completeWithAccount(_ account: AccountModel){
+            completion?(.success(account))
+        }
+        
     }
     
     class LoadingViewSpy: LoadingView {
