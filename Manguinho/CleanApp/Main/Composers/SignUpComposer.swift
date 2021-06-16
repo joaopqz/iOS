291336cde7c396fb6e356e01b571f19931278c5f
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-16 11:34:25 -0300

    add environment variables

--- Manguinho/CleanApp/Main/Composers/SignUpComposer.swift
+++ Manguinho/CleanApp/Main/Composers/SignUpComposer.swift
@@ -9,7 +9,7 @@ public final class SignUpComposer {
     public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController{
         let controller = SignUpViewController.instantiate()
         let emailValidatorAdapter = EmailValidatorAdapter()
-        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: controller)
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
         controller.signUp = presenter.signUp
         return controller
     }
