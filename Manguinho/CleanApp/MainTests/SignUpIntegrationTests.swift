32173b0df7bd0f08e42cb603297d5c30971891c8
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-16 11:40:23 -0300

    remove unused code

--- Manguinho/CleanApp/MainTests/SignUpIntegrationTests.swift
+++ Manguinho/CleanApp/MainTests/SignUpIntegrationTests.swift
@@ -4,9 +4,6 @@ import Main
 class SignUpIntegrationTests: XCTestCase {
 
     func test_ui_presentation_integration(){
-        debugPrint("==================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("==================")
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
