d24e32cdf96b1211a3881a1c99f039b61c01688a
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-21 10:54:52 -0300

    ensure LoginPresenter completes with erro if Authentication completes with error

--- Manguinho/CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
+++ Manguinho/CleanApp/Presentation/Presenters/SignUp/SignUpPresenter.swift
@@ -15,16 +15,24 @@ public final class SignUpPresenter {
         self.validation = validation
     }
     
-    public func signUp(viewModel: SignUpViewModel){
+    public func signUp(viewModel: SignUpViewModel) {
         if let message = validation.validate(data: viewModel.toJson()) {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
-        } else{
-            loadingView.display(viewModel: LoadingViewModel(isLoading: true ))
+        } else {
+            loadingView.display(viewModel: LoadingViewModel(isLoading: true))
             addAccount.add(addAccountModel: viewModel.toAddAccountModel()) { [weak self] result in
                 guard let self = self else { return }
                 self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
-                switch result{
-                case .failure: self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: "Algo inesperado aconteceu, tente novamente em alguns instantes!!"))
+                switch result {
+                case .failure(let error):
+                    var errorMessage: String!
+                    switch error {
+                    case .emailInUse:
+                        errorMessage = "Esse e-mail já está em uso."
+                    default:
+                        errorMessage = "Algo inesperado aconteceu, tente novamente em alguns instantes!!"
+                    }
+                    self.alertView.showMessage(viewModel: AlertViewModel(title: "Erro", message: errorMessage))
                 case .success: self.alertView.showMessage(viewModel: AlertViewModel(title: "Sucesso", message: "Conta criada com sucesso!!"))
                 }
             }
