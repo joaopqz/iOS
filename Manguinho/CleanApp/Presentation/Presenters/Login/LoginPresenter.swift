c6fc40df0d0198cad720ab79a1e8d01d52bcc943
joao.queiroz <joao.queiroz@gerencianet.com.br>
2021-06-21 10:07:29 -0300

    ensure LoginPresenter calls Authentication with correct values

--- Manguinho/CleanApp/Presentation/Presenters/Login/LoginPresenter.swift
+++ Manguinho/CleanApp/Presentation/Presenters/Login/LoginPresenter.swift
@@ -3,17 +3,20 @@ import Domain
 
 public final class LoginPresenter {
     private let alertView: AlertView
-
     private let validation: Validation
+    private let authentication: Authentication
     
-    public init (alertView: AlertView, validation: Validation){
+    public init (alertView: AlertView, validation: Validation, authentication: Authentication){
         self.alertView = alertView
         self.validation = validation
+        self.authentication = authentication
     }
     
     public func login(viewModel: LoginViewModel){
         if let message = validation.validate(data: viewModel.toJson()) {
             alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
+        } else{
+            authentication.auth(authenticationModel: viewModel.toAuthenticationModel(), completion: {_ in})
         }
     }
 }
